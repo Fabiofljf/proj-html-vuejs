c1ed3bb3af1abd5eb058f8ad518f1bb56a678daf
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-14 15:00:00 +0200

    Add Main - CardPost

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="site_footer">
-    <div id="footer" class="bg-footer">
+    <div id="footer">
       <div class="container">
         <div class="row special_padding">
           <div class="col">
@@ -78,7 +78,7 @@
       </div>
     </div>
     <!-- /#footer -->
-    <div id="info_footer" class="bg-info_footer">
+    <div id="info_footer">
       <div class="container">
         <div class="row">
           <div class="col d-flex justify-content-between align-items-center">
