2e2e9bd684f49ac9eac1829803899de94a3b2c39
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-14 12:22:07 +0200

    Adding Main - component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <SiteHeader />
+    <SiteMain />
     <SiteFooter />
   </div>
 </template>
 
 <script>
 import SiteHeader from "./components/SiteHeader.vue";
-//import SiteMain from './components/SiteMain.vue'
+import SiteMain from './components/SiteMain.vue'
 import SiteFooter from "./components/SiteFooter.vue";
 
 export default {
   name: "App",
   components: {
     SiteHeader,
-    //SiteMain,
+    SiteMain,
     SiteFooter,
   },
 };
